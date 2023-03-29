45db7b493e57d559f81bd801d80a1b44a89e4a73
Rodrigo <rodribola.0886@gmail.com>
2023-03-29 10:45:38 +0100

    add menu with options

--- game.js
+++ game.js
@@ -8,7 +8,7 @@ var config = {
     scale: {
         mode: Phaser.Scale.FIT,
         parent: 'flappy bird',
-        autoCenter: Phaser.Scale.CENTER_BOTH,
+        autoCenter: Phaser.Scale.CENTER_BOTH
     },
     physics: {
         default: 'arcade',
@@ -18,6 +18,7 @@ var config = {
         }
     },
     scene: {
+        key: 'main',
         preload: preload,
         create: create,
         update: update
@@ -25,7 +26,7 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-var isPaused = false, gameOver = false;
+var isPaused = false, gameOver = false, start = false;
 var score = 0;
 var score2 = 0;
 var birdX = (gameWidth/2)-50;
@@ -36,6 +37,11 @@ function preload (){
     this.load.image('pipetop', 'assets/pipetop.png');
     this.load.image('bird', 'assets/bird.png');
     this.load.image('bird2', 'assets/bird2.png');
+    this.load.image('background', 'assets/back.png');
+    this.load.image('start', 'assets/start.png');
+    this.load.image('easyMode', 'assets/easyMode.png');
+    this.load.image('mediumMode', 'assets/mediumMode.png');
+    this.load.image('hardMode', 'assets/hardMode.png');
 
     this.load.audio('flap', './assets/sounds/wing.ogg');
     this.load.audio('hit', './assets/sounds/hit.ogg');
@@ -45,35 +51,65 @@ function preload (){
 }
 
 var platforms,spacebar,player,player2,scoreText, scoreText2;
-var gap = 250; 
+var gap = 0; 
 var xGap = 250;
 
-/*
-    Nivel facil -> gap = 500
-    Nivel Medio -> gap = 150
-    Nivel Dificil -> gap = 120
-
-    Cheat presionar uma tecla e aumenta o pipe
-*/
-
 function create (){
+    const startButton = this.add.image(800, 400, 'background');
+    const startLabel = this.add.image(800, 250, 'start');
+    const easyModeLabel = this.add.image(800, 400, 'easyMode');
+    const mediumModeLabel = this.add.image(800, 500, 'mediumMode');
+    const hardModeLabel = this.add.image(800, 600, 'hardMode');
+    
+    startLabel.setInteractive();
+    easyModeLabel.setInteractive();
+    mediumModeLabel.setInteractive();
+    hardModeLabel.setInteractive();
+
+    easyModeLabel.on('pointerdown', function(){
+        easyModeLabel.destroy();
+        mediumModeLabel.destroy();
+        hardModeLabel.destroy();
+        gap = 300;
+    })
+
+    mediumModeLabel.on('pointerdown', function(){
+        easyModeLabel.destroy();
+        mediumModeLabel.destroy();
+        hardModeLabel.destroy();
+        gap = 160;
+    })
+
+    hardModeLabel.on('pointerdown', function(){
+        easyModeLabel.destroy();
+        mediumModeLabel.destroy();
+        hardModeLabel.destroy();
+        gap = 130;
+    })
+
+    startLabel.on('pointerdown', function(){
+        startButton.destroy();
+        startLabel.destroy();
+        start = true;
+    }) 
+
     var colors = ["0x1fbde0","0x0a4957","0x08272e"];
     var randColor = colors[Math.floor(Math.random() * colors.length)];
-    this.cameras.main.setBackgroundColor(randColor)
+    this.cameras.main.setBackgroundColor(randColor);
 
-    scoreText = this.add.text(birdX, (gameHeight/4),score,{fontSize: 60, color: '#fff'});
+    scoreText = this.add.text(birdX, (gameHeight/4),score,{fontSize: 60, color: '#d9d9d9'});
     scoreText2 = this.add.text(birdX, (gameHeight/2),score2,{fontSize: 60, color: '#d9d9d9'});
-    
+
     platforms = this.physics.add.staticGroup();
-    var pipePos = gameWidth+2*xGap
-    let pos = getRandom();
-    
+    var pipePos = gameWidth+2*xGap;
+    let pos = [100, 400]; // Replace this with the getRandom function
+
     platforms.create(pipePos, pos[0], 'pipebottom').setScale(1).refreshBody();
-    platforms.create(pipePos, pos[1], 'pipetop').setScale(1).refreshBody();
+    platforms.create(pipePos, pos[1], 'pipetop').setScale(1).refreshBody(); 
 
     player = this.physics.add.sprite(birdX, birdY, 'bird');
     player2 = this.physics.add.sprite(birdX, birdY, 'bird2');
-
+    
     player.setBounce(0.2);
     player.setCollideWorldBounds(true);
 
@@ -95,7 +131,7 @@ function create (){
     });
 
     player.body.setGravityY(300)
-    player2.body.setGravityY(150)
+    player2.body.setGravityY(300)
 
     this.physics.add.collider(player, platforms, playerHit, null, game)
     this.physics.add.collider(player2, platforms, playerHit2, null, game)
@@ -127,9 +163,10 @@ function getRandom() {
 
 var countpipe = 0;
 function update ()  {
+
     let children = platforms.getChildren();
     children.forEach((child) => {
-        if (child instanceof Phaser.GameObjects.Sprite) {
+        if (child instanceof Phaser.GameObjects.Sprite && start) {
             child.refreshBody();
             child.x += -3;
             if(child.x <= gameWidth && !child.drawn) {
@@ -143,12 +180,14 @@ function update ()  {
                     platforms.create(gameWidth+xGap, pos[1], 'pipetop').setScale(1).refreshBody();
                     countpipe=0;
                 }
+
             }
+
             if(child.x <= -50) {
                 child.destroy();
             }
 
-            if(child.x< birdX && !gameOver && child.texture.key=="pipebottom" && !child.scored){
+            if(child.x< birdX && !gameOver && child.texture.key=="pipebottom" && !child.scored && start){
                 child.scored = true
                 score+=1;
                 score2+=1;
